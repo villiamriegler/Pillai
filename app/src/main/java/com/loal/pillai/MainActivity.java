13a131dc5731611342fde96d69d9361b55f5f007
Firverior02 <alexanderrunebou02@gmail.com>
2023-12-20 19:02:59 +0100

    feat: add fragments for different pages

--- app/src/main/java/com/loal/pillai/MainActivity.java
+++ app/src/main/java/com/loal/pillai/MainActivity.java
@@ -1,14 +1,48 @@
 package com.loal.pillai;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        replaceFragment(new ScannerFragment());
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setBackground(null);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch(item.getItemId()) {
+                case R.id.help:
+                    replaceFragment(new HelpFragment());
+                    break;
+                case R.id.account:
+                    replaceFragment(new AccountFragment());
+                    break;
+                case R.id.scan:
+                    replaceFragment(new ScannerFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
